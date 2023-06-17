62d2453edae53bdb54e1c0b3a0fb22540ce1cbb2
eleventh <allan@celedonio.digital>
2023-06-17 21:01:17 +1200

    Added axios and passed recaptcha token

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,60 @@
 import React, { 
-  useState, useRef, useEffect 
+  useState
 } from 'react';
 import ReCAPTCHA from "react-google-recaptcha";
+import CryptoJS from "crypto-js";
 import Axios from 'axios';
 import './App.css';
 
-
 function App() {
- 
-  const ReCaptchaKey = process.env.REACT_APP_RECAPTCHA_SITE_KEY;  
-  
+
+  const Apikey = process.env.REACT_APP_API_KEY;  
+  const ApiUrl = process.env.REACT_APP_API_URL; 
+  const ReCaptchaKey = process.env.REACT_APP_RECAPTCHA_SITE_KEY;    
+  const PublicUrl = new URL(process.env.PUBLIC_URL!, window.location.toString());
+   
+  const [enquiryName, setEnquiryName] = useState("");
   const [enquiryEmail, setEnquiryEmail] = useState("");
   const [enquirySubject, setEnquirySubject] = useState("");
   const [enquiryMessage, setEnquiryMessage] = useState("");   
   const [enquiryToken, setEnquiryToken] = useState((value) => {
     return value;
   });   
-  
+    
   const handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();  
-    if (enquiryEmail.length && enquiryEmail.length && enquiryMessage.length) {
-      console.log(enquiryEmail);
-      console.log(enquirySubject);
-      console.log(enquiryMessage);  
-      console.log(enquiryToken);    
+    if (enquiryEmail.length && enquiryEmail.length && enquiryMessage.length) {      
+      var enquiryUrl = ApiUrl + 'sendmail';      
+      var enquiryData = JSON.stringify([{
+        pkey: Apikey,
+        host: PublicUrl.host,
+        body: {
+          nme: enquiryName,
+          eml: enquiryEmail,
+          sbj: enquirySubject,
+          msg: enquiryMessage,
+          tkn: enquiryToken
+        }
+      }]);
+      var enquiryEncr = CryptoJS.AES.encrypt(enquiryData, Apikey).toString();      
+      try {
+        Axios.post(enquiryUrl, {
+          payload: enquiryEncr
+        },{
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+            'Access-Control-Allow-Credentials': false,
+            'Referrer-Host': PublicUrl.host
+          }
+        })
+        .then((response) => {
+          console.log(response);
+        });         
+      } catch (error) {
+        console.error(error);
+      }
     }
   }     
   
@@ -58,7 +89,7 @@ function App() {
           </a>
         </div>
         <div className="box shrink-0 pt-[1.5em]">
-          <img src="/slide.png" alt="slide" className="slide lg:h-auto lg:max-w-full" />
+          <img src="/pc.png" alt="slide" className="slide lg:h-auto lg:max-w-full" />
         </div>
         <div className="box content-center shrink-1 p-[0 auto] lg:p-[0.5em] lg:text-left">
           <h1 className="
@@ -92,8 +123,20 @@ function App() {
               Need website/app quotation or any enquiry about Celedonio.Digital? Let us know.
             </p>
             <form action="#" className="space-y-8" onSubmit={handleSubmit}>
+              <div>                    
+                    <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-200 text-left">Your Fullname</label>
+                    <input type="name" 
+                      value={enquiryName}
+                      onChange={(e) => setEnquiryName(e.target.value)}
+                      id="name" className="
+                      shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+                      focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 
+                      dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                      dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+                      placeholder="John Doe" required />
+                </div>
                 <div>                    
-                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-200 text-left">Your email</label>
+                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-200 text-left">Your Email</label>
                     <input type="email" 
                       value={enquiryEmail}
                       onChange={(e) => setEnquiryEmail(e.target.value)}
@@ -102,7 +145,7 @@ function App() {
                       focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 
                       dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
                       dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
-                      placeholder="email@url.com" required />
+                      placeholder="jonh.doe@email.com" required />
                 </div>
                 <div>
                     <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-200 text-left">Subject</label>
@@ -117,7 +160,7 @@ function App() {
                       placeholder="Let us know how we can help you" required />
                 </div>
                 <div className="sm:col-span-2">
-                    <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-200 text-left">Your message</label>
+                    <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-200 text-left">Your Message</label>
                     <textarea 
                       value={enquiryMessage}
                       onChange={(e) => setEnquiryMessage(e.target.value)}
@@ -125,7 +168,7 @@ function App() {
                       block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border 
                       border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 
                       dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 
-                      dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+                      dark:focus:border-primary-500" placeholder="Leave your enquiry here..."></textarea>
                 </div>
                 <div className="sm:col-span-2 px-[1em] lg:px-[14em]">
                   <ReCAPTCHA                    
